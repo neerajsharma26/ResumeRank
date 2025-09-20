b000304a56b5023f98ceb25fa912fd97b745968c
Raj Aryan Pratap Singh <raj.aryan@varaheanalytics.com>
2025-09-20 12:02:28 +0000

    again image is not visible

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image';
 import * as React from 'react';
 
 export const Logo = ({ className }: { className?: string }) => (
-    <Image src="/images/varahe-logo.png" alt="Hire Varahe Logo" width={40} height={40} className={className} />
+    <img src="/images/varahe-logo.png" alt="Varahe Logo" width={40} height={40} className={className} />
 );
