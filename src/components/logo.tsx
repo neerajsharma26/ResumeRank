0d77ebea531d6ed801e01162c6a215eecc634f62
Raj Aryan Pratap Singh <raj.aryan@varaheanalytics.com>
2025-09-20 10:00:04 +0000

    again logo will not apper

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,5 @@ import Image from 'next/image';
 import * as React from 'react';
 
 export const Logo = ({ className }: { className?: string }) => (
-    <div className={className}>
-        <Image src="/images/varahe-logo.png" alt="Hire Varahe Logo" width={40} height={40} />
-    </div>
+    <Image src="/images/varahe-logo.png" alt="Hire Varahe Logo" width={40} height={40} className={className} />
 );
